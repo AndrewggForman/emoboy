9c348dff91088354b3c9990a724aed816498ba1c
Tristan Gill <tristan.gill@mail.com>
2025-07-06 11:32:23 -0400

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod registers;
 fn main() {
     println!("Hello, world!");
     println!("Hello from my-branch");
+    println!("i like turtles");
     
     let mut registers = Registers::new();
     registers.write_byte(RegByte::A, 0x1);
