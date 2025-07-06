59976b5c883540530ae275d967fcd3524f7194f8
Tristan Gill <tristan.gill@mail.com>
2025-07-06 11:23:24 -0400

    experimenting with branches

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod registers;
 
 fn main() {
     println!("Hello, world!");
+    println!("Hello from my-branch");
     
     let mut registers = Registers::new();
     registers.write_byte(RegByte::A, 0x1);
